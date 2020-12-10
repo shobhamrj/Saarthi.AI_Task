15d95312b6e1366cfcc86618164eb77c6d61dbe5
Shobhm Rajak <shobham.rajak@gmail.com>
2020-12-10 23:56:05 +0530

    added skeleton pages

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const PORT = process.env.PORT | 3000;
+const app = express();
+app.use(cors());
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + '/login.html'))
+})
+
+app.get("/url", (req, res) => {
+    res.sendFile(path.join(__dirname + '/url.html'))
+})
+
+app.listen(PORT);
