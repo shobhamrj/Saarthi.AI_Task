9e5ea95b61db25734d1f7ef670cf34ad857d043e
shobhamrj <shobham.rajak@gmail.com>
2020-12-11 12:07:14 +0530

    final

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 const app = express();
+app.use(express.static('public'))
 app.use(cors());
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + '/login.html'))
---
+++ public/js/client.js
@@ -0,0 +1,5 @@
+function openTab() {
+    let url = document.getElementById("url-in").value
+    console.log(url)
+    window.open(url, '__blank')
+}
