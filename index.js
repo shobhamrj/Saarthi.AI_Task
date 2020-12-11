38cb8275ff98211b924a61f6ccfed59c97469c1a
shobhamrj <shobham.rajak@gmail.com>
2020-12-11 11:08:45 +0530

    bug fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.get("/", (req, res) => {
